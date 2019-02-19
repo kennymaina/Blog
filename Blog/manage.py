4ef885582bc6d9f9e295c732bfc90c8e8ccfc19c
kennymaina <victormurigi@outlook.com>
2019-02-19 12:38:42 +0300

    imports in manage .py

--- Blog/manage.py
+++ Blog/manage.py
@@ -1,7 +1,7 @@
-from app import create_app,db
-from flask_script import Manager, Server
-from app.models import User,Role,Blog, Comments
-from  flask_migrate import Migrate, MigrateCommand
+# from app import create_app,db
+# from flask_script import Manager, Server
+# from app.models import User,Role,Blog, Comments
+# from  flask_migrate import Migrate, MigrateCommand
 
 '''
 creating app instance
