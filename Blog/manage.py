af8387e23b2371e2da642c2d3de26eab30efcb68
kennymaina <victormurigi@outlook.com>
2019-02-19 12:40:23 +0300

    creating app instance

--- Blog/manage.py
+++ Blog/manage.py
@@ -1,16 +1,16 @@
-# from app import create_app,db
-# from flask_script import Manager, Server
-# from app.models import User,Role,Blog, Comments
-# from  flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User,Role,Blog, Comments
+from  flask_migrate import Migrate, MigrateCommand
 
-'''
-creating app instance
-'''
-app= create_app('development')
+# '''
+# creating app instance
+# '''
+# app= create_app('development')
 
-manager = Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('server',Server)
+# manager = Manager(app)
+# migrate = Migrate(app,db)
+# manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
 
 @manager.command
