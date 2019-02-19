2a2ca1fcf72e0b74fb51096eb17a85c81ea2f428
kennymaina <victormurigi@outlook.com>
2019-02-19 14:07:18 +0300

    imports in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap
-from config import config_options
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
-from flask_mail import Mail
-from flask_moment import Moment
-from flask_simplemde import SimpleMDE
+# from flask import Flask
+# from flask_bootstrap import Bootstrap
+# from config import config_options
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# from flask_uploads import UploadSet,configure_uploads,IMAGES
+# from flask_mail import Mail
+# from flask_moment import Moment
+# from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
