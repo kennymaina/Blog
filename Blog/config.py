6927105cb4516ffee58ae0fee1620d9778a6e3ab
kennymaina <victormurigi@outlook.com>
2019-02-19 12:37:18 +0300

    modification n config.py

--- Blog/config.py
+++ Blog/config.py
@@ -5,34 +5,34 @@ class Config():
     parent class config
     '''
 
-    QUOTES = 'https://quotesapi.org/v2/sources?category={}&apiKey={}'
-    # NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    QUOTES_API_KEY = os.environ.get('QUOTES_API_KEY')
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/blog'
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SECRET_KEY=os.environ.get('SECRET_KEY') 
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    # QUOTES = 'https://quotesapi.org/v2/sources?category={}&apiKey={}'
+    # # NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    # QUOTES_API_KEY = os.environ.get('QUOTES_API_KEY')
+    # # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/blog'
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # #  email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # SECRET_KEY=os.environ.get('SECRET_KEY') 
+    # # simple mde  configurations
+    # SIMPLEMDE_JS_IIFE = True
+    # SIMPLEMDE_USE_CDN = True
     
     @staticmethod
     def init_app(app):
         pass
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-#     pass
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/blog'
-#     DEBUG = True
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/blog'
+    DEBUG = True
 
 
 class TestConfig(Config):
